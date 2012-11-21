964417dbcac1f37a2798c92ce4c50ef2f58a302c
Nick Aceves <nickaceves@gmail.com>
2012-11-21 04:05:15 -0600

    Implemented parser.

--- lisp.js
+++ lisp.js
@@ -9,3 +9,24 @@ lispjs.tokenize = function(input) {
 			return c != '';
 		});
 };
+
+lispjs.parse = function(input) {
+	var list = [];
+
+	if (input[0] != '(')
+		throw 'Expected "("';
+	
+	for(var i = 1; i < input.length; i++) {
+		if (input[i] == ')')
+			return list;
+		else if (input[i] == '(') {
+			var sub = lispjs.parse(input.slice(i));
+			i += sub.length;
+			list.push(sub);
+		}
+		else 
+			list.push(input[i]);
+	}
+	
+	throw 'Expected ")"';
+};
