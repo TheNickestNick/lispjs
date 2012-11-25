b0667f02de9d11b8fb743aa43b67dab24d1ffe6e
Nick Aceves <nickaceves@gmail.com>
2012-11-25 03:03:22 -0600

    Refactoring evaluation. Fixed bug where global environment was affecting the window object.

--- lisp.js
+++ lisp.js
@@ -1,10 +1,12 @@
 function Symbol(name) { this.name = name; };
 
-function Environment(parent, symbols) {
-	this.symbols = symbols || {};
-
+function Environment(parent) {
+	this.symbols = {};
+	
 	this.get = function(name) {
-		return this.symbols[name] || (parent && parent.get(name));
+		return this.symbols[name] 
+			|| (parent instanceof Environment && parent.get(name))
+			|| (parent && parent[name]);
 	};
 	
 	this.set = function(name, val) {
@@ -13,7 +15,7 @@ function Environment(parent, symbols) {
 };
 
 function atom(str) {
-	if (str[0] === '"' && str[str.length - 1] == '"')
+	if ((str[0] === '"' && str[str.length - 1] == '"') || (str[0] === "'" && str[str.length - 1] == "'"))
 		return str.substring(1, str.length - 1);
 
 	return parseFloat(str) || new Symbol(str);
@@ -54,6 +56,34 @@ var lispjs = new (function(){
 		}
 	};
 	
+	function define(args, body, env) {
+		var name, value;
+		
+		if (args instanceof Symbol) {
+			name = args.name;
+			value = evaluate(body, env);
+		}
+		else if (args instanceof Array) {
+			name = args.shift().name;
+			var argSymbols = args;
+			value = lambda(argSymbols, body, env)
+		}
+		
+		env.set(name, value);
+		return value;
+	};
+	
+	function lambda(argSymbols, body, env) {
+		return function() {
+			var callEnv = new Environment(env);
+			
+			for (var i = 0; i < arguments.length; i++)
+				callEnv.set(argSymbols[i].name, arguments[i]);
+
+			return evaluate(body, callEnv);
+		};
+	};
+	
 	function evaluate(ast, env) {
 		if (ast instanceof Symbol) {
 			return env.get(ast.name);
@@ -65,10 +95,7 @@ var lispjs = new (function(){
 			
 		if (ast[0] instanceof Symbol) {
 			if (ast[0].name === 'define') {
-				var symbol = ast[1];
-				var value = evaluate(ast[2], env);
-				env.set(symbol.name, value);
-				return value;
+				return define(ast[1], ast[2], env);
 			}
 			else if (ast[0].name === 'if') {
 				return evaluate(ast[1], env) 
@@ -76,17 +103,7 @@ var lispjs = new (function(){
 					 : evaluate(ast[3], env);
 			}
 			else if (ast[0].name === 'lambda') {
-				var argSymbols = ast[1];
-				var body = ast[2];
-				
-				return function() {
-					var callEnv = new Environment(env);
-					
-					for (var i = 0; i < arguments.length; i++)
-						callEnv.set(argSymbols[i].name, arguments[i]);
-
-					return evaluate(body, callEnv);
-				};
+				return lambda(ast[1], ast[2], env);
 			}
 		}
 		
@@ -95,14 +112,14 @@ var lispjs = new (function(){
 			list.push(evaluate(ast[i], env));
 		
 		var procedure = list.shift();
-		return procedure.apply({ env: env }, list);
+		return procedure.apply(null, list);
 	};
 	
 	this.parse = parse;	
 	this.tokenize = tokenize;
 	this.evaluate = evaluate;
 	
-	var globals = new Environment(null, window);
+	var globals = new Environment(window);
 	globals.set('+', function(x, y) { return x + y; });
 	globals.set('-', function(x, y) { return x - y; });
 	globals.set('*', function(x, y) { return x * y; });
@@ -110,6 +127,7 @@ var lispjs = new (function(){
 	globals.set('=', function(x, y) { return x == y; });
 	globals.set('>', function(x, y) { return x > y; });
 	globals.set('<', function(x, y) { return x < y; });
+	globals.set('@', function(x, y) { return x[y]; });
 
 	this.globals = globals;
 	this.interpret = function(str) {
